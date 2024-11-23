4ab3b64921a6531e2a6463019cb68af753fbc4a0
Evgenii44r <evgenii44r@gmail.com>
2024-11-23 15:37:33 +0300

    add

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ x_finish = w - 50
 def key_handler(event):
     canvas.move(player1_id, 10, 0)
 window = Tk()
-window.title("огони меня если сможешь")
+window.title("Догони меня если сможешь")
 canvas = Canvas(window, width=w, height=h, bg="white")
 canvas.pack()
 player1_id = canvas.create_rectangle(x1, y1, x1 + player_size, y1 + player_size, fill=player1_color)
